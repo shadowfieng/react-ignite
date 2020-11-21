2da26456118d06c5082447e6ad0eef61f67d8b1a
shadowfieng <shadowfieng@gmail.com>
2020-11-22 00:02:32 +0500

    implemented thunk

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-/* Redux setup */
 
-import { combineReducers, createStore } from 'redux';
-import gamesReducer from './reducers/gamesReducer';
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-const store = createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
